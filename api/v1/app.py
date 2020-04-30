bd10cd9b6dc8f43d3b5c7d5b114c74989cbb8114
fruizga <ruizgallego@gmail.com>
2020-04-29 22:51:58 -0500

    Error handler for JSON response

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ first status API """
 
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -15,6 +15,12 @@ app.register_blueprint(app_views)
 def close_ss(self):
     """ close session storage """
     storage.close()
+
+
+@app.errorhandler(404)
+def error_404(self):
+    """404 Error"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
